64f8dfef0edc85872abbac7835112beb3dbecf4e
Ararat <melkumyan2077@gmail.com>
2023-05-23 13:28:58 +0300

    update

--- src/index.jsx
+++ src/index.jsx
@@ -3,5 +3,5 @@ import * as ReactDOM from "react-dom";
 import { Header } from "./Header";
 
 window.addEventListener("load", () => {
-    ReactDOM.render(<Header />, document.getElementById("root"));
+  ReactDOM.render(<Header />, document.getElementById("root"));
 });
