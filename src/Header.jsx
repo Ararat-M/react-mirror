2f36efa216b60340cd4d27f165a00ffe23bb6175
Ararat <melkumyan2077@gmail.com>
2023-05-23 14:08:10 +0300

    update

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,10 @@
 import * as React from "react";
+import classes from "./header.module.css"
 
 export function Header() {
   return (
     <header>
-      <h1> Hello React </h1>
+      <h1 className={[classes.example, classes.background].join(' ')} > Hello React </h1>
     </header>
   )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -14,10 +14,24 @@ module.exports = {
     filename: "index.js"
   },
   module: {
-    rules: [{
+    rules: [
+    {
       test: /\.[tj]sx?$/,
       use: ["ts-loader"]
-    }]
+    },
+    {
+      test: /\.css$/,
+      use: ["style-loader", {
+        loader: "css-loader",
+        options: {
+          modules: {
+            mode: "local",
+            localIdentName: "[name]__[local]--[hash:base64:5]"
+          }
+        }
+      }]
+    },
+  ]
   },
   plugins: [
     new HtmlWebpackPlugin({ template: path.resolve(__dirname, "index.html" )})
